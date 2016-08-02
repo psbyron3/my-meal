b49366cde8efa2d0ea19aef26df7b13968631ab7
Phil Seidel <psbyron3@gmail.com>
2016-08-02 10:51:03 -0700

    [add] added NavBar to app render

--- client/components/app.js
+++ client/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-
+import NavBar from './navbar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <div>
           {this.props.children}
         </div>
