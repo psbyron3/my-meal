ab6701702ab25a06596bc63675b00f699549295a
Phil Seidel <psbyron3@gmail.com>
2016-08-02 19:36:49 -0700

    [feature] Implemented boilerplate for reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
+import UserInfoReducer from './user_info_reducer';
+import UserHistory from './user_history_reducer';
+import MapReducer from './map_reducer';
+import FilteredReducer from './filtered_reducer';
+import SelectedEvent from './selected_event_reducer';
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state,
+  userInfo        : UserInfoReducer,
+  userHistory     : UserHistory,
+  map             : MapReducer,
+  filteredReducer : FilteredReducer,
+  selectedEvent   : SelectedEvent
 });
 
 export default rootReducer;
