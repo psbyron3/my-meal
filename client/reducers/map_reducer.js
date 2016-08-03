cb055e02bb8d405715a11fc7b32e5956d3bae25a
Phil Seidel <psbyron3@gmail.com>
2016-08-03 13:50:45 -0700

    [feature] Initialized location search for map

--- client/reducers/map_reducer.js
+++ client/reducers/map_reducer.js
@@ -1,9 +1,10 @@
+import { SEARCH_LOCATIONS } from '../actions/index.js'
 
 
 export default function (state = [], action) {
   switch (action.type) {
-
-
+    case SEARCH_LOCATIONS:
+      return action.payload;
     default:
       return state;
   }
