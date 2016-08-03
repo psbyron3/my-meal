26aba31fc15badc92a518fd92f1e1905650a7dc1
Phil Seidel <psbyron3@gmail.com>
2016-08-03 16:52:55 -0700

    [git] Fixed merge conflicts

--- client/index.js
+++ client/index.js
@@ -9,9 +9,7 @@ import App from './components/app';
 import reducers from './reducers';
 import routes from './routes';
 
-
 const store = applyMiddleware(reducers, window.devToolsExtension ? window.devToolsExtension() : f => f);
-
 
 ReactDOM.render(
   <Provider store={store}>
