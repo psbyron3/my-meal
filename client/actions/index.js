467815b09df0a8d52d2b54e8a8aecd0144a04889
Phil Seidel <psbyron3@gmail.com>
2016-08-05 13:48:36 -0700

    [feature] Completed getAllInRadius to ping map api as well as our api to return matches in 5 mile radius.

--- client/actions/index.js
+++ client/actions/index.js
@@ -45,7 +45,7 @@ export function convertAddress(searchParams) {
           console.log('error searching location from actions searchLocation', err);
         }
       });
-      browserHistory.push('home')
+    browserHistory.push('home');
   };
 }
 
