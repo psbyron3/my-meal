ce36b93a45bc7906930cd85abc05eab09ac0e351
Phil Seidel <psbyron3@gmail.com>
2016-08-05 13:49:41 -0700

    [refactor] Got rid of home button

--- client/components/landing.js
+++ client/components/landing.js
@@ -7,7 +7,6 @@ export default class Landing extends Component {
     return (
       <div>On the Landing Page
         <div className="text-xs-right">
-          <Link to="/home" className="btn btn-primary">Home</Link>
         </div>
       </div>
 
