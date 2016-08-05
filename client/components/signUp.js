b249c4b7fabd4067b829c05ad799c5eecf66e544
LycheeXD <lycheeexd@gmail.com>
2016-08-04 23:01:30 -0700

    [feature] initialize sign up view

---
+++ client/components/signUp.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+export default class SignIn extends Component {
+  render() {
+    return (
+      <div className="top-margin">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-md-offset-4">
+              <form className="form-signin">
+                <fieldset>
+                  <h3 className="form-signin-heading">Sign Up</h3>
+                  <input type="text" className="form-control" placeholder="Email Address" />
+
+                  <input type="password" className="form-control" placeholder="Password" />
+
+                  <button className="btn btn-md btn-primary btn-block" type="submit">Sign Up</button>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
