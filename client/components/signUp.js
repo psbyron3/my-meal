5fbecbd4c98f7104e310842407bc4c97efd835c9
LycheeXD <lycheeexd@gmail.com>
2016-08-05 01:53:35 -0700

    [feature] add name, address, phone field for sign up

--- client/components/signUp.js
+++ client/components/signUp.js
@@ -12,7 +12,11 @@ class SignUp extends Component {
 
 
   render() {
-    const { fields: { email,
+    const { fields: { firstName,
+                      lastName,
+                      address,
+                      phone,
+                      email,
                       password,
                     }, handleSubmit } = this.props;
 
@@ -30,6 +34,22 @@ class SignUp extends Component {
                     <fieldset>
                       <h3 className="form-signin-heading">Sign Up</h3>
                       <div className="text-help">
+                        <div className={`form-group ${firstName.touched && firstName.invalid ? 'has-danger' : ''}`}>
+                          <input type="text" className="form-control" placeholder="First Name" {...firstName} />
+                        </div>
+
+                        <div className={`form-group ${lastName.touched && lastName.invalid ? 'has-danger' : ''}`}>
+                          <input type="text" className="form-control" placeholder="Last Name" {...lastName} />
+                        </div>
+
+                        <div className={`form-group ${address.touched && address.invalid ? 'has-danger' : ''}`}>
+                          <input type="text" className="form-control" placeholder="Address" {...address} />
+                        </div>
+
+                        <div className={`form-group ${phone.touched && phone.invalid ? 'has-danger' : ''}`}>
+                          <input type="text" className="form-control" placeholder="Phone Number" {...phone} />
+                        </div>
+
                         <div className={`form-group ${email.touched && email.invalid ? 'has-danger' : ''}`}>
                           <input type="text" className="form-control" placeholder="Email Address" {...email} />
                         </div>
@@ -61,6 +81,22 @@ class SignUp extends Component {
 const validate = (values) => {
   const errors = {};
 
+  if (!values.firstName) {
+    errors.firstName = 'Please enter first name';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Please enter last name';
+  }
+
+  if (!values.address) {
+    errors.address = 'Please enter a valid address';
+  }
+
+  if (!values.phone) {
+    errors.phone = 'Please enter a phone number';
+  }
+
   if (!values.email) {
     errors.email = 'Please enter valid email';
   }
@@ -74,7 +110,11 @@ const validate = (values) => {
 
 export default reduxForm({
   form: 'SignUpForm',
-  fields: ['email',
+  fields: ['firstName',
+           'lastName',
+           'address',
+           'phone',
+           'email',
            'password'],
   validate,
 })(SignUp);
