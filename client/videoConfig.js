16d3182c7a63d53b2e51b934d897096916061ca3
Nizar Ayari <nizarayari17@gmail.com>
2016-08-05 20:10:25 -0700

    [feature]adding a background video to the landing page

---
+++ client/videoConfig.js
@@ -0,0 +1,58 @@
+
+  function scaleVideoContainer() {
+    const height = $(window).height() + 5;
+    const unitHeight = `${parseInt(height)}px`;
+    $('.homepage-hero-module').css('height', unitHeight);
+  }
+
+  function initBannerVideoSize(element) {
+    $(element).each(function () {
+      $(this).data('height', $(this).height());
+      $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+  }
+
+  function scaleBannerVideoSize(element) {
+    const windowWidth = $(window).width();
+    const windowHeight = $(window).height() + 5;
+    let videoWidth;
+    let videoHeight;
+
+    console.log(windowHeight);
+
+    $(element).each(function () {
+      const videoAspectRatio = $(this).data('height') / $(this).data('width');
+
+      $(this).width(windowWidth);
+
+      if (windowWidth < 1000) {
+        videoHeight = windowHeight;
+        videoWidth = videoHeight / videoAspectRatio;
+        $(this).css({ 'margin-top': 0, 'margin-left': `${-(videoWidth - windowWidth) / 2}px` });
+
+        $(this).width(videoWidth).height(videoHeight);
+      }
+
+      $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+    });
+  }
+
+  $(document).ready(function () {
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function () {
+      scaleVideoContainer();
+      scaleBannerVideoSize('.video-container .poster img');
+      scaleBannerVideoSize('.video-container .filter');
+      scaleBannerVideoSize('.video-container video');
+    });
+
+    $('.fillWidth').get(0).play();
+  });
+
