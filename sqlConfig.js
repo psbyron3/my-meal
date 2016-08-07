76faf651a7b6f8ccccb39d7d166ff0ec73fcc315
Phil Seidel <psbyron3@gmail.com>
2016-08-07 00:47:59 -0700

    [bug] Solved issue with .env not being recognized. Path corrected.

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,9 +1,10 @@
 var path = require('path');
+var config = require('./envConfig.js');
 
 module.exports = {
 
   development: {
-    user     : process.env.db_username,
+    user     : process.env.db_username, 
     password : process.env.db_password,
     database : 'mymeal_db',
     connection: {
