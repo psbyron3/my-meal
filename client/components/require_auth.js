852699b07392aa5195851875b6630055d1e4df3d
LycheeXD <lycheeexd@gmail.com>
2016-08-08 20:18:58 -0700

    [feature] allow sign in persistence on refresh

--- client/components/require_auth.js
+++ client/components/require_auth.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
 // let authCheck = JSON.parse(localStorage["reduxPersist:auth"])
 // console.log("AUTHCHECKER: ", authCheck);
+=======
+const authCheck = JSON.parse(localStorage['reduxPersist:auth']);
+console.log('AUTHCHECKER: ', authCheck);
+>>>>>>> [feature] allow sign in persistence on refresh
 
 export default function (ComposedComponent) {
   class Authentication extends Component {
@@ -10,14 +15,24 @@ export default function (ComposedComponent) {
       router: React.PropTypes.object,
     }
 
+<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
     componentDidMount() {
       if (!this.props.authenticated) {
+=======
+    componentWillMount() {
+      if (!this.props.authenticated && !authCheck.authenticated) {
+>>>>>>> [feature] allow sign in persistence on refresh
         this.context.router.push('/');
       }
     }
 
+<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
     componentDidUpdate(nextProps) {
       if (!nextProps.authenticated) {
+=======
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated && !authCheck.authenticated) {
+>>>>>>> [feature] allow sign in persistence on refresh
         this.context.router.push('/');
       }
     }
@@ -35,9 +50,13 @@ export default function (ComposedComponent) {
     // }
 
     render() {
+<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
       // console.log("CHECKHERERRR: ", !authCheck.authenticated)
       // console.log("WHAT IS AUTH?? ", this.props.authenticated)
       // console.log("NEXTTTTT ", nextProps.authenticated)
+=======
+      console.log('CHECKHERERRR: ', !authCheck.authenticated);
+>>>>>>> [feature] allow sign in persistence on refresh
       return <ComposedComponent {...this.props} />;
     }
   }
---
+++ client/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import thunk from 'redux-thunk';
+import { persistStore, autoRehydrate } from 'redux-persist';
+
+import App from './components/app';
+import reducers from './reducers';
+import routes from './routes';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+const store = createStoreWithMiddleware(reducers, 
+  window.devToolsExtension ? window.devToolsExtension() : f => f, autoRehydrate());
+
+persistStore(store);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.getElementById('app'));
