b69b52a3d6d92c2be5b7fd3e271247ce70404bd1
LycheeXD <lycheeexd@gmail.com>
2016-08-09 11:22:11 -0700

    [git] merge conflict

--- client/components/require_auth.js
+++ client/components/require_auth.js
@@ -1,38 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
-// let authCheck = JSON.parse(localStorage["reduxPersist:auth"])
-// console.log("AUTHCHECKER: ", authCheck);
-=======
-const authCheck = JSON.parse(localStorage['reduxPersist:auth']);
-console.log('AUTHCHECKER: ', authCheck);
->>>>>>> [feature] allow sign in persistence on refresh
-
 export default function (ComposedComponent) {
   class Authentication extends Component {
     static contextTypes = {
       router: React.PropTypes.object,
     }
 
-<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
     componentDidMount() {
       if (!this.props.authenticated) {
-=======
-    componentWillMount() {
-      if (!this.props.authenticated && !authCheck.authenticated) {
->>>>>>> [feature] allow sign in persistence on refresh
         this.context.router.push('/');
       }
     }
 
-<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
     componentDidUpdate(nextProps) {
       if (!nextProps.authenticated) {
-=======
-    componentWillUpdate(nextProps) {
-      if (!nextProps.authenticated && !authCheck.authenticated) {
->>>>>>> [feature] allow sign in persistence on refresh
         this.context.router.push('/');
       }
     }
@@ -50,13 +32,6 @@ export default function (ComposedComponent) {
     // }
 
     render() {
-<<<<<<< 7362d78a14fa7016ad07e57c774385373952956c
-      // console.log("CHECKHERERRR: ", !authCheck.authenticated)
-      // console.log("WHAT IS AUTH?? ", this.props.authenticated)
-      // console.log("NEXTTTTT ", nextProps.authenticated)
-=======
-      console.log('CHECKHERERRR: ', !authCheck.authenticated);
->>>>>>> [feature] allow sign in persistence on refresh
       return <ComposedComponent {...this.props} />;
     }
   }
