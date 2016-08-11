6c479ff4299099ac8529707f24df381fcd889972
Nizar Ayari <nizarayari17@gmail.com>
2016-08-10 22:29:51 -0700

    [feature] render message by sent or receive

--- client/components/messageEntry.js
+++ client/components/messageEntry.js
@@ -2,15 +2,27 @@ import React from 'react';
 import moment from 'moment';
 
 const MessageEntry = ({ body }) => {
-//   if( localStorage.getItem('userId') === body.userId ) {
-  return (
-    <div className="row msg_container base_sent">
-      <div className="col-md-10 col-xs-10">
-        <div className="messages msg_sent">
-          <p>{body.content}</p>
-          <time dateTime="2016-08-10T13:00"> {body.userName} - {moment(body.createdAt).fromNow()} </time>
+  if (localStorage.getItem('userId') === body.userId) {
+    return (
+      <div className="row msg_container base_sent">
+        <div className="col-md-10 col-xs-10">
+          <div className="messages msg_sent">
+            <p>{body.content}</p>
+            <time dateTime="2016-08-10T13:00"> {body.userName} - {moment(body.createdAt).fromNow()} </time>
+          </div>
+        </div>
+        <div className="col-md-2 col-xs-2 avatar">
+          <img
+            alt=""
+            src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg"
+            className="img-responsive"
+          />
         </div>
       </div>
+    );
+  }
+  return (
+    <div className="row msg_container base_receive">
       <div className="col-md-2 col-xs-2 avatar">
         <img
           alt=""
@@ -18,26 +30,14 @@ const MessageEntry = ({ body }) => {
           className="img-responsive"
         />
       </div>
+      <div className="col-md-10 col-xs-10">
+        <div className="messages msg_sent">
+          <p>{body.content}</p>
+          <time dateTime="2016-08-10T13:00"> {body.userName} - {moment(body.createdAt).fromNow()} </time>
+        </div>
+      </div>
     </div>
   );
-//   } else {
-//     return (
-//       <div className="row msg_container base_receive">
-//         <div className="col-md-2 col-xs-2 avatar">
-//           <img
-//            alt=""
-//            src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg"
-//            className="img-responsive"/>
-//         </div>
-//         <div className="col-md-10 col-xs-10">
-//           <div className="messages msg_sent">
-//             <p>Good Bye</p>
-//             <time datetime="2016-08-10T13:00"> Joe 56mn </time>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
 };
 
 export default MessageEntry;
