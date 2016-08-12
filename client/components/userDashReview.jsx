c04c7516aacebee87cfc21c65b8614e4747d0df2
Phil Seidel <psbyron3@gmail.com>
2016-08-11 17:17:25 -0700

    [feature] Initialized user dash landing. This is where users will be able to rate chefs.

---
+++ client/components/userDashReview.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import { Col, FormControl, textarea } from 'react-bootstrap';
+
+export default class userReview extends Component {
+
+  setRating () => {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Col className="review-gutter" md={2} />
+        <Col className="review-container" md={8}>
+          <div className="review-event-basics">
+            <div className="review-event-image">
+              <img src="../assets/stock-chef.jpg" /> 
+            </div>
+            <div className="review-event-details">
+              <div className="review-event-title">
+                Some dope meal
+              </div>
+              <div className="review-event-date">
+                Some arbitrary date
+              </div>
+            </div>
+          </div>
+          <div className="review-rating-container">
+            <div className="review-rating" onClick={this.setRating()}>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+            </div>
+          </div>
+          <div className="review-comments">
+            <textarea />
+          </div>
+        </Col>
+        <Col className="review-gutter" md={2}/>
+      </div>
+
+    );
+  }
+}
---
+++ client/components/userDashView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import UserReview from './userDashReview';
+
+export default class userDashView extends Component {
+
+  render() {
+    return (
+      <div>
+
+        <UserReview />
+
+      </div>
+
+    );
+
+  }
+}
