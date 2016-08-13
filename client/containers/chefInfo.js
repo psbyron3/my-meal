20ea4fd2b8f2ece3c72d269e33710dbef8b046e1
LycheeXD <lycheeexd@gmail.com>
2016-08-13 07:56:17 -0700

    [feature] display chef average rating

--- client/containers/chefInfo.js
+++ client/containers/chefInfo.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { reviewAverage } from '../utils/helper';
+
+const _ = require('lodash');
 
 class ChefInfo extends Component {
+
+
   render() {
+    let chefRatingArray = [];
+
+    _.each(this.props.chefEvents, (event) => {
+      if(typeof event.rating === 'number') {
+        chefRatingArray.push(event.rating);
+      }
+    })
+
+    let chefRating = reviewAverage(chefRatingArray);
+
     return (
       <div>
-        hello?
+        chef rating: {chefRating}
       </div>
     );
   }
