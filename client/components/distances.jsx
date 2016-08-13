daafa280d6125e5e337307dd435521e96f9d0cd5
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-08-13 10:05:02 -0700

    [feature] Add genre and distance dropdowns to advanced search

---
+++ client/components/distances.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const DistanceMenu = ({onDistanceChange}) => {
+
+  return (
+    <select onChange={onDistanceChange}>
+      <option selected disabled>Select a radius</option>
+      <option value={1}>1 miles</option>
+      <option value={5}>5 miles</option>
+      <option value={10}>10 miles</option>
+      <option value={15}>15 miles</option>
+      <option value={20}>20 miles</option>
+      <option value={25}>25 miles</option>
+    </select>
+  )
+}
+
+export default DistanceMenu;
