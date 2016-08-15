f26aa57400c28110501d01fca27c4262a883d541
Nizar Ayari <nizarayari17@gmail.com>
2016-08-15 16:17:23 -0700

    [feature] upload profile picture

--- client/actions/index.js
+++ client/actions/index.js
@@ -78,7 +78,7 @@ export const SignInFunc = (props) => {
   };
 };
 
-export const SignUpFunc = (props) => {
+export const SignUpFunc = (props, userPic) => {
   const firstName = props.firstName;
   const lastName = props.lastName;
   const address = props.address;
@@ -87,35 +87,51 @@ export const SignUpFunc = (props) => {
   const email = props.email;
   const password = props.password;
 
-  return (dispatch) => {
-    return axios({
-      method: 'POST',
-      url: '/api/auth/signup',
-      data: {
-        firstName,
-        lastName,
-        address,
-        phoneNumber,
-        userName,
-        email,
-        password,
-      },
-    })
-      .then((response) => {
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('id', response.data.result.id);
-        dispatch({
-          type: AUTH_USER,
-        });
-        browserHistory.push('/');
-      })
-      .catch((err) => {
-        console.log('ERROR', err);
-        dispatch({
-          type: AUTH_ERROR,
-        });
-      });
+  const data = new FormData();
+  data.append('file', userPic[0]);
+  const opts = {
+    transformRequest() { return data; },
   };
+
+  axios.post('/api/event/picture', data, opts)
+    .then((resp) => {
+      const url = resp.data;
+      console.log(url, 'SUPPOSED URL');
+      return url;
+    })
+    .then((url) => {
+      return axios({
+        method: 'POST',
+        url: '/api/auth/signup',
+        data: {
+          firstName,
+          lastName,
+          address,
+          phoneNumber,
+          userName,
+          email,
+          password,
+          userPic: url,
+        },
+      })
+        .then((response) => {
+          console.log('SIGN UP PAYLOOOOOOOOAAAAD: ', response);
+          return (dispatch) => {
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('id', response.data.result.id);
+            dispatch({
+              type: AUTH_USER,
+            });
+            browserHistory.push('/');
+          };
+        })
+        .catch((err) => {
+          console.log('ERROR', err);
+          return {
+            type: AUTH_ERROR,
+          };
+        });
+    });
 };
 
 export const SignOutFunc = () => {
