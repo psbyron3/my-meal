e20be6f00f3d384b9f777b4d4ca155075798c9be
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-08-19 19:31:08 -0700

    [feature] Connected findReviewsByUser to endpoint in reviews controller

---
+++ client/components/userReviewList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PastReview from './userPastReview';
+
+class ReviewList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (<div></div>);
+  }
+}
+
+export default ReviewList;
