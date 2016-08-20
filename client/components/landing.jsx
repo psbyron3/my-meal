c5b039600b2a3db001393229ed07d2a9c134ff83
Nizar Ayari <nizarayari17@gmail.com>
2016-08-19 22:24:21 -0700

    [bug]keep rendering video without refreshing page

--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -1,8 +1,65 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import $ from 'jquery';
 
 export default class Landing extends Component {
 
+  componentDidMount(){
+    // Video configuration
+    function scaleVideoContainer() {
+      const height = $(window).height() + 5;
+      const unitHeight = `${parseInt(height, 10)}px`;
+      $('.homepage-hero-module').css('height', unitHeight);
+    }
+
+    function scaleBannerVideoSize(element) {
+      const windowWidth = $(window).width();
+      const windowHeight = $(window).height() + 5;
+      let videoWidth;
+      let videoHeight;
+
+
+      $(element).each(function () {
+        const videoAspectRatio = $(this).data('height') / $(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if (windowWidth < 1000) {
+          videoHeight = windowHeight;
+          videoWidth = videoHeight / videoAspectRatio;
+          $(this).css({ 'margin-top': 0, 'margin-left': `${-(videoWidth - windowWidth) / 2}px` });
+
+          $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+      });
+    }
+
+    function initBannerVideoSize(element) {
+      $(element).each(function () {
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+      });
+
+      scaleBannerVideoSize(element);
+    }
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', () => {
+      scaleVideoContainer();
+      scaleBannerVideoSize('.video-container .poster img');
+      scaleBannerVideoSize('.video-container .filter');
+      scaleBannerVideoSize('.video-container video');
+    });
+
+    $('.fillWidth').get(0).play();
+  }
 
   render() {
     return (
@@ -12,7 +69,6 @@ export default class Landing extends Component {
             <div className="filter"></div>
             <video className="fillWidth" autoPlay muted loop>
               <source src="https://s3-us-west-2.amazonaws.com/mymealmks/videos/523395704.mp4" type="video/mp4" />
-              <source src="https://s3-us-west-2.amazonaws.com/mymealmks/videos/523395704.webm" type="video/webm" />
             </video>
             <div className="poster hidden">
               <img src="https://s3-us-west-2.amazonaws.com/mymealmks/lac+NZ.jpg" alt="" />
