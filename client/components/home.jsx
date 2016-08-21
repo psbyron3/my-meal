82a8c88dad0c3ef41738050d76ff255791100a24
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-08-21 01:36:18 -0700

    [feature] Add message for no search results

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Button, Grid, Row, Col } from 'react-bootstrap';
-import MapView from '../containers/map.jsx';
-import MapList from '../containers/maplist.jsx';
-import JoinModal from '../containers/join-modal.jsx';
+import MapView from '../containers/map';
+import MapList from '../containers/maplist';
+import JoinModal from '../containers/join-modal';
+import ErrorAlert from './errorAlert';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      alert: false,
       showModal: false,
       hoverEvent: 0,
     };
@@ -15,6 +17,7 @@ class Home extends Component {
     this.setHoverEvent = this.setHoverEvent.bind(this);
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.showAlert = this.showAlert.bind(this);
   }
 
   setHoverEvent(index) {
@@ -35,12 +38,19 @@ class Home extends Component {
     });
   }
 
+  showAlert() {
+    this.setState({
+      alert: true
+    });
+  }
+
   render() {
     return (
       <div>
-        <MapView setHoverEvent={this.setHoverEvent} openModal={this.openModal} />
+        <MapView setHoverEvent={this.setHoverEvent} openModal={this.openModal} showAlert={this.showAlert} />
         <MapList hoverEvent={this.state.hoverEvent} openModal={this.openModal} />
         <JoinModal showModal={this.state.showModal} closeModal={this.closeModal} />
+        <ErrorAlert alert={this.state.alert} />
       </div>
     );
   }
