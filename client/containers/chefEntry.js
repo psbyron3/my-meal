1f0e5507138ebb8fa10cd18e3f94ffe8b18f685d
Phil Seidel <psbyron3@gmail.com>
2016-08-22 03:16:09 -0700

    [style] Styled chef dash

--- client/containers/chefEntry.js
+++ client/containers/chefEntry.js
@@ -3,15 +3,31 @@ import React, { PropTypes } from 'react';
 const ChefPastEntry = (props) => {
   return (
     <div className="card-mc">
-      {props.eventId}
-      <br />
-      {props.eventName}
-      <br />
-      <div>
-        <button onClick={() => { props.clicked(props.eventId, props.eventName); }}> chat </button>
+      <div className="chef-event-name">
+        {props.eventName}
       </div>
-      <div>
-        <button onClick={() => { props.deleteEvent(props.eventId); }}> delete </button>
+      <br />
+      <div className="chef-meal-image">
+        <img
+          src={props.image}
+          role="presentation"
+        />
+      </div>
+      <div className="chef-btn-container">
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => { props.clicked(props.eventId, props.eventName); }}
+          > chat
+          </button>
+        </div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => { props.deleteEvent(props.eventId); }}
+          > delete
+          </button>
+        </div>
       </div>
     </div>
   );
