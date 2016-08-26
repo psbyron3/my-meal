a7439c252104c74b667924339b204f249e8af0a0
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-08-25 18:15:30 -0700

    [style] Move searchbar next to icon in navbar-header div; change collapse behavior

--- server/controllers/reviewCtrl.js
+++ server/controllers/reviewCtrl.js
@@ -4,35 +4,6 @@ const url = require('url');
 const db = require('../db/db.js');
 
 module.exports = {
-  '/users/:userId': {
-    get(req, res) {
-      console.log('Received GET at /api/review/users/:userId');
-      console.log('url is.....', req.url);
-      const userId = url.parse(req.url, true).path.slice(7);
-      console.log('parsing url............', url.parse(req.url, true).path);
-      console.log('userId in /api/review/users/:userId is...', userId);
-      Review.findReviewsByUser(userId)
-        .then((results) => {
-          console.log('results of findReviewsByUser', results);
-          res.send(results);
-        })
-        .catch((err) => {
-          res.send(err);
-        });
-    },
-    post(req, res) {
-      console.log('Received POST at /api/review/:userId');
-      res.end('Received POST at /api/review/:userId');
-    },
-    put(req, res) {
-      console.log('Received PUT at /api/:review');
-      res.end('Received PUT at /api/:review');
-    },
-    delete(req, res) {
-      console.log('Received DELETE at /api/:review');
-      res.end('Received DELETE at /api/:review');
-    },
-  },
   '/': {
     get(req, res) {
       console.log('Received GET at /api/review');
@@ -52,6 +23,35 @@ module.exports = {
           console.log('error in POST at /api/review', err);
           res.send(err);
         });
+    },
+    put(req, res) {
+      console.log('Received PUT at /api/:review');
+      res.end('Received PUT at /api/:review');
+    },
+    delete(req, res) {
+      console.log('Received DELETE at /api/:review');
+      res.end('Received DELETE at /api/:review');
+    },
+  },
+  '/users/:userId': {
+    get(req, res) {
+      console.log('Received GET at /api/review/users/:userId');
+      console.log('url is.....', req.url);
+      const userId = url.parse(req.url, true).path.slice(7);
+      console.log('parsing url............', url.parse(req.url, true).path);
+      console.log('userId in /api/review/users/:userId is...', userId);
+      Review.findReviewsByUser(userId)
+      .then((results) => {
+        console.log('results of findReviewsByUser', results);
+        res.send(results);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+    },
+    post(req, res) {
+      console.log('Received POST at /api/review/:userId');
+      res.end('Received POST at /api/review/:userId');
     },
     put(req, res) {
       console.log('Received PUT at /api/:review');
